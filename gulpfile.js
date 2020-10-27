70816ea3371e5ef58265af68760b0cdbb66effb6
ay <shlemafona@gmail.com>
2020-10-27 15:46:57 +0200

    Release v1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
-// Gulp modules
-//const gulp = require('gulp');
+//////////////////////////////////////////////////////////////////////////
+// INCLUDE 
+//////////////////////////////////////////////////////////////////////////
+
+// Gulp core methods
 const { watch, series, parallel, src, dest} = require('gulp');
 
-const fileinclude = require('gulp-file-include');
+const fs = require('fs');
 const del = require('del');
+
+// Plugins
+
+const fileinclude = require('gulp-file-include');
 const scss = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const groupcssmediaqueries = require('gulp-group-css-media-queries');
@@ -11,12 +18,25 @@ const cleancss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const uglifyjs = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
+// webp 
+const webp = require('gulp-webp');
+const webphtml = require('gulp-webp-html');
+const webpcss = require('gulp-webp-css');
+// fonts
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
 
+//////////////////////////////////////////////////////////////////////////
+// CONSTS
+//////////////////////////////////////////////////////////////////////////
+
+// Project paths
 const PATH_PREFIX_BUILD = "build";
-const PATH_BUILD = "./build/";
-const PATH_PREFIX_SRC = "src";
+const PATH_BUILD        = "./"+PATH_PREFIX_BUILD+"/";
+const PATH_PREFIX_SRC   = "src";
 
 const PROJECT_PATH = {  
+
   build: { 
       html:  PATH_PREFIX_BUILD + '/',
       js:    PATH_PREFIX_BUILD + '/js/',
@@ -24,6 +44,7 @@ const PROJECT_PATH = {
       img:   PATH_PREFIX_BUILD + '/img/',
       fonts: PATH_PREFIX_BUILD + '/fonts/'
   },
+
   src: { 
       html:  PATH_PREFIX_SRC + '/*.html', 
       js:    PATH_PREFIX_SRC + '/js/*.js',
@@ -31,138 +52,180 @@ const PROJECT_PATH = {
       img:   PATH_PREFIX_SRC + '/img/**/*.*', 
       fonts: PATH_PREFIX_SRC + '/fonts/**/*.*'
   },
+
   watch: { 
       html:  PATH_PREFIX_SRC + '/**/*.html',
       js:    PATH_PREFIX_SRC + '/js/*.js',
       css:   PATH_PREFIX_SRC + '/css/*.scss',
       img:   PATH_PREFIX_SRC + '/img/**/*.*',
       fonts: PATH_PREFIX_SRC + '/fonts/**/*.*'
-  },
-  clean: PATH_BUILD
+  },  
+
+  clean: PATH_BUILD,
+  browserSyncBaseDir: PATH_BUILD
+
 };
 
-//////////////////////////////////////////////////////////////////////////
-// Browser sync 
-//////////////////////////////////////////////////////////////////////////
+// Plugins options
+const PLUGINS_OPTIONS = {
 
-const objBrowserSync = require("browser-sync").create();
-
-function browserSyncStart(params) {
-
-  const pluginParams = {
-    server: {
-      baseDir: PATH_BUILD
+  browserSync: {
+    server: {      
+      baseDir: PROJECT_PATH.browserSyncBaseDir // -> ./build/
     },
     port: 3000,
     notify: false
-  };
+  },
 
-  objBrowserSync.init(
-    pluginParams
-  );
+  // gulp-sass
+  scss: {
+    outputStyle: "expanded"
+  },
+
+  // gulp-autoprefixer
+  autoprefixer: {    
+    cascade: true,
+    overrideBrowserslist: ['last 5 versions'],
+    grid: "autoplace"
+  },
+
+  // gulp-imagemin
+  imagemin: {
+    progressive: true,
+    svgoPlugins: [{ removeViewBox: false }],
+    interlaced: true,
+    optimizationLevel: 3
+  },
+
+  webp: {
+    quality: 70
+  }
 
 }
 
 //////////////////////////////////////////////////////////////////////////
-// Src build
+// GLOBAL OBJECTS
 //////////////////////////////////////////////////////////////////////////
+
+const objBrowserSync = require("browser-sync").create();
+
+//////////////////////////////////////////////////////////////////////////
+// Src build API
+//////////////////////////////////////////////////////////////////////////
+
+function clearBuild() {
+     
+  return del(PROJECT_PATH.clean);
+
+}
 
 function buildHtml() {    
 
   return src(PROJECT_PATH.src.html)
     .pipe(fileinclude())
+    .pipe(webphtml())
     .pipe(dest(PROJECT_PATH.build.html))
     .pipe(objBrowserSync.stream());
 
 }
 
-function buildCss() {
-
-  const scssOpts = {
-    outputStyle: "expanded"
-  };
-
-  const autoprefixerOpts = {
-    overrideBrowsersList: ["last 9 versions"],
-    cascade: true
-  };
-
-  const renameOpts = {
-    extname: ".min.css"
-  };
-
+function buildCss() {  
+ 
+  const scssOpts = PLUGINS_OPTIONS.scss;
+  const autoprefixerOpts = PLUGINS_OPTIONS.autoprefixer;
+  const renameOpts = {extname: ".min.css"};
+  
   return src(PROJECT_PATH.src.css)    
-    .pipe(scss(scssOpts))
-    .pipe(groupcssmediaqueries())
-    //.pipe(autoprefixer())
-    .pipe(autoprefixer(autoprefixerOpts))
-    .pipe(dest(PROJECT_PATH.build.css))
+    .pipe(scss(scssOpts))    
+    .pipe(groupcssmediaqueries())        
+    .pipe(autoprefixer(autoprefixerOpts))        
+    .pipe(webpcss())
+    .pipe(dest(PROJECT_PATH.build.css))        
+    // Minify and copy to *.min.css
     .pipe(cleancss())
     .pipe(rename(renameOpts))
-    .pipe(dest(PROJECT_PATH.build.css))
+    .pipe(dest(PROJECT_PATH.build.css))   
     .pipe(objBrowserSync.stream());
 
 }
 
 function buildJs() {    
 
-  const renameOpts = {
-    extname: ".min.js"
-  };
+  const renameOpts = {extname: ".min.js"};
 
   return src(PROJECT_PATH.src.js)
     .pipe(fileinclude())
     .pipe(dest(PROJECT_PATH.build.js))
     .pipe(uglifyjs())
+    // Minify and copy to *.min.js
     .pipe(rename(renameOpts))
     .pipe(dest(PROJECT_PATH.build.js))
     .pipe(objBrowserSync.stream());
 
 }
 
-
 function buildImg() {    
+  
+  const imageminOpts = PLUGINS_OPTIONS.imagemin;
+  const webpOpts = PLUGINS_OPTIONS.webp;
 
-  const imageminOpts = {
-    progressive: true,
-    svgoPlugins: [{ removeViewBox: false }],
-    interlaced: true,
-    optimizationLevel: 3
-  };
-
-  return src(PROJECT_PATH.src.img)    
+  return src(PROJECT_PATH.src.img) 
+    // webp
+    .pipe(webp(webpOpts))
+    .pipe(dest(PROJECT_PATH.build.img))    
+    // normal img
+    .pipe(src(PROJECT_PATH.src.img))
     .pipe(imagemin(imageminOpts))
     .pipe(dest(PROJECT_PATH.build.img))
     .pipe(objBrowserSync.stream());
 
 }
 
+function buildFonts() {
+
+  // ttf2wof
+  return src(PROJECT_PATH.src.fonts)
+    .pipe(ttf2woff())    
+    .pipe(dest(PROJECT_PATH.build.fonts))    
+    // ttf2wof2
+    .pipe(src(PROJECT_PATH.src.fonts))
+    .pipe(ttf2woff2())    
+    .pipe(dest(PROJECT_PATH.build.fonts))
+    .pipe(objBrowserSync.stream());
 
 
-//////////////////////////////////////////////////////////////////////////
-// Watch
-//////////////////////////////////////////////////////////////////////////
-
-function clearBuild() {
-  return del(PROJECT_PATH.clean);
 }
 
+//////////////////////////////////////////////////////////////////////////
+// Local web server with live reload API
+//////////////////////////////////////////////////////////////////////////
+
+function browserSyncStart(params) {
+
+  objBrowserSync.init(PLUGINS_OPTIONS.browserSync);
+ 
+ }  
+
 function watchAndBuild() {
+
   watch(PROJECT_PATH.watch.html, buildHtml);
   watch(PROJECT_PATH.watch.css, buildCss);
   watch(PROJECT_PATH.watch.js, buildJs);
   watch(PROJECT_PATH.watch.img, buildImg);
+  watch(PROJECT_PATH.watch.fonts, buildFonts);
+
 }
 
+
 //////////////////////////////////////////////////////////////////////////
-// Tasks
+// TASKS
 //////////////////////////////////////////////////////////////////////////
 
-let buildAllTask = series(clearBuild, 
-    parallel(buildCss, buildJs, buildImg, buildHtml)
+let rebuildAllTask = series(clearBuild, 
+    parallel(buildCss, buildJs, buildImg, buildFonts, buildHtml)
 );
 
-let watchTask = parallel(browserSyncStart, buildAllTask, watchAndBuild);
+let watchTask = parallel(browserSyncStart, rebuildAllTask, watchAndBuild);
 
-exports.build = buildAllTask;
+exports.rebuild = rebuildAllTask;
 exports.default = watchTask;
